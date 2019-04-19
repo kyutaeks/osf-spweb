48c216ef6352bb662d963fbcf6973eb7365cc4b4
lgt8879 <lgt8879@gmail.com>
2019-04-19 17:43:11 +0900

    reCommit

---
+++ src/main/java/com/osf/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.osf.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String goHome() {
+		return "index";
+	}
+}
