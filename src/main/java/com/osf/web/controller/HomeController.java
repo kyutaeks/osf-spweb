28519c5383e79aa2135a591cee5ae37c8ce6a9c4
lgt8879 <lgt8879@gmail.com>
2019-04-22 10:10:08 +0900

    Spring linux Commit

--- src/main/java/com/osf/web/controller/HomeController.java
+++ src/main/java/com/osf/web/controller/HomeController.java
@@ -1,6 +1,10 @@
 package com.osf.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,7 +16,12 @@ public class HomeController {
 	}
 
 	@RequestMapping("/test")
-	public String goTest() {
+	public String goTest(Model model) {
+		List<String> list = new ArrayList<String>();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		model.addAttribute("list", list);
 		return "test";
 	}
 }
