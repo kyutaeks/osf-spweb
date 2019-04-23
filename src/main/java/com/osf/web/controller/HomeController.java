f2e9c270229863ccb3602b4191a38a930c348f32
lgt8879 <lgt8879@gmail.com>
2019-04-23 17:51:11 +0900

    19.04.23 Commit mybatis

--- src/main/java/com/osf/web/controller/HomeController.java
+++ src/main/java/com/osf/web/controller/HomeController.java
@@ -1,22 +1,38 @@
 package com.osf.web.controller;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class HomeController {
+	@Autowired
+	private DataSource ds;
 
 	@RequestMapping("/")
 	public String goHome() {
 		return "index";
 	}
 
-	@RequestMapping("/test")
+	@RequestMapping(value="/test",method=RequestMethod.GET)
 	public String goTest(Model model) {
+		try {
+			log.info("con=>{}", ds.getConnection());
+		} catch (SQLException e) {
+			
+		}
+		model.addAttribute("msg", "hello~");
 		List<String> list = new ArrayList<String>();
 		list.add("1");
 		list.add("2");
